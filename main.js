aba8fa9df96be6d199465af1b57d655193538db7
bynned <benny.cascarino@tuni.fi>
2023-06-19 15:13:07 +0300

    Hah, watched an old tutorial thats outdated

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const Discord = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 require('dotenv').config();
 
-const client = new Discord.Client({ intents: [
-    Discord.GatewayIntentBits.Guilds,
-    Discord.GatewayIntentBits.GuildMessages
-  ]});
-
-
-client.once('ready', () => {
-    console.log('KymppiBotti Online!');
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
 });
 
 client.login(process.env.BOT_TOKEN);
-
