c9c4c57c604b2f0588131690d2d1fb454a344495
bynned <benny.cascarino@tuni.fi>
2023-06-19 13:07:03 +0300

    Initial working version

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client({ intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages
+  ]});
+
+
+client.once('ready', () => {
+    console.log('KymppiBotti Online!');
+});
+
+client.login(process.env.BOT_TOKEN);
 
