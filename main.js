149b88b1b3002bffcb068059ef42660112c1ac9e
bynned <benny.cascarino@tuni.fi>
2023-06-19 15:30:44 +0300

    Answers bro with bro

--- main.js
+++ main.js
@@ -10,4 +10,15 @@ const client = new Client({
     ],
 });
 
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is ready`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return; // So that it doesn't spam "bro" forever
+    if (message.content === 'bro' || message.content === 'äijä') {
+        message.reply('bro');
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
