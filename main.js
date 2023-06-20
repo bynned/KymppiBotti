57246237a497ad4e783438e9c6d4b35b8c41522f
bynned <benny.cascarino@tuni.fi>
2023-06-20 11:31:54 +0300

    Commands

--- main.js
+++ main.js
@@ -28,4 +28,18 @@ client.on('messageCreate', (message) => {
     }
 });
 
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+    commands.push(command);
+}
+
 client.login(process.env.BOT_TOKEN);
